3a6f6669579699aec34449dd43fc0e22db8a950f
= <s_munsey@live.com>
2015-03-23 15:39:50 +0000

    someth

---
+++ src/main/java/com/smunsey/portfolio/LoginController.java
@@ -0,0 +1,15 @@
+package com.smunsey.portfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public String loginHandler(){
+		     
+		return "welcome";
+	}
+}
